00f7c3b7d5ab2ad5a0660d100b4fbe91e639999b
Jamie Diner <jamiediner@Jamies-MacBook-Pro.local>
2017-02-22 21:15:30 -0500

    project working

--- main.py
+++ main.py
@@ -279,8 +279,17 @@ def time_to_class(trip, class_start_time):
     delta_minutes = (class_seconds - trip_seconds)/60
     return delta_minutes
 
-def add_missing_hours(aggregated_data, day_hour, keys):
+def add_missing_hours(aggregated_data):
     complete_array = []
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    hours = range(24)
+    day_hour = {}
+    keys = []
+    for d in days:
+        for h in hours:
+            day_hour[(d,h)]=0
+            keys.append((d,h))
+
     for a in aggregated_data:
         index = (a[0],a[1])
         count = a[2]
@@ -295,15 +304,6 @@ def add_missing_hours(aggregated_data, day_hour, keys):
 #####################################################################################################
 #############                           END    FUNCTIONS                                #############
 #####################################################################################################
-
-days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-hours = range(24)
-day_hour = {}
-keys = []
-for d in days:
-    for h in hours:
-        day_hour[(d,h)]=0
-        keys.append((d,h))
 
 local_time_departure = "CONVERT_TZ(FROM_UNIXTIME(double_departure/1000,'%Y-%m-%d %H:%i:%s'), '+00:00','-05:00')"
 local_time_arrival = "CONVERT_TZ(FROM_UNIXTIME(double_arrival/1000,'%Y-%m-%d %H:%i:%s'), '+00:00','-05:00')"
@@ -375,32 +375,38 @@ hour_of_day = "HOUR(CONVERT_TZ(FROM_UNIXTIME(timestamp/1000,'%Y-%m-%d %H:%i:%s')
 activity_name = "'walking'"
 order_by_weekday = "FIELD(Weekday, 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY')"
 query = "SELECT {0} AS Weekday, {1} AS Hour, COUNT(*) FROM {2} WHERE activity_name = {3} GROUP BY {0},{1} ORDER BY {4}, Hour;".format(day_of_week, hour_of_day, _ACTIVITY, activity_name, order_by_weekday)
-walking_aggregated = make_query(cursor,query)
-walking_aggregated = add_missing_hours(walking_aggregated, day_hour, keys)
+walking_aggregated_missing = make_query(cursor,query)
+
+queries_missing = [{'query':query, 'results':walking_aggregated_missing}]
+walking_aggregated = add_missing_hours(walking_aggregated_missing)
 
 queries = queries + [{'query':query, 'results':walking_aggregated}]
 
 #Still
 activity_name = "'still'"
 query = "SELECT {0} AS Weekday, {1} AS Hour, COUNT(*) FROM {2} WHERE activity_name = {3} GROUP BY {0},{1} ORDER BY {4}, Hour;".format(day_of_week, hour_of_day, _ACTIVITY, activity_name, order_by_weekday)
-still_aggregated = make_query(cursor,query)
-still_aggregated = add_missing_hours(still_aggregated, day_hour, keys)
+still_aggregated_missing = make_query(cursor,query)
+queries_missing = queries_missing + [{'query':query, 'results':still_aggregated_missing}]
+
+still_aggregated = add_missing_hours(still_aggregated_missing)
 
 queries = queries + [{'query':query, 'results':still_aggregated}]
 
 #Running
 activity_name = "'running'"
 query = "SELECT {0} AS Weekday, {1} AS Hour, COUNT(*) FROM {2} WHERE activity_name = {3} GROUP BY {0},{1} ORDER BY {4}, Hour;".format(day_of_week, hour_of_day, _ACTIVITY, activity_name, order_by_weekday)
-running_aggregated = make_query(cursor,query)
-running_aggregated = add_missing_hours(running_aggregated, day_hour, keys)
+running_aggregated_missing = make_query(cursor,query)
+queries_missing = queries_missing + [{'query':query, 'results':running_aggregated_missing}]
+running_aggregated = add_missing_hours(running_aggregated_missing)
 
 queries = queries + [{'query':query, 'results':running_aggregated}]
 
 #Vehicle
 activity_name = "'in_vehicle'"
 query = "SELECT {0} AS Weekday, {1} AS Hour, COUNT(*) FROM {2} WHERE activity_name = {3} GROUP BY {0},{1} ORDER BY {4}, Hour;".format(day_of_week, hour_of_day, _ACTIVITY, activity_name, order_by_weekday)
-vehicle_aggregated = make_query(cursor,query)
-vehicle_aggregated = add_missing_hours(vehicle_aggregated, day_hour, keys)
+vehicle_aggregated_missing = make_query(cursor,query)
+queries_missing = queries_missing + [{'query':query, 'results':vehicle_aggregated_missing}]
+vehicle_aggregated = add_missing_hours(vehicle_aggregated_missing)
 
 queries = queries + [{'query':query, 'results':vehicle_aggregated}]
 
@@ -433,7 +439,8 @@ def about():
 @app.route('/quality')
 def quality():
     template = JINJA_ENVIRONMENT.get_template('templates/quality.html')
-    return template.render()
+    context = {'queries':queries_missing}
+    return template.render(context)
 
 @app.errorhandler(404)
 def page_not_found(e):
